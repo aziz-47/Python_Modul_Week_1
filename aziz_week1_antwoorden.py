157a4bde703281b5dd1d14d4b97f719dac9d116a
Aziz Eke <67024992+aziz-47@users.noreply.github.com>
2026-01-12 22:19:45 +0100

    Add movie input and display functionality

---
+++ aziz_week1_antwoorden.py
@@ -0,0 +1,13 @@
+vraa1: liste = []
+
+for i in range(3):
+    movie = input(f"{i+1}. favori filmi girin: ")
+    liste.append(movie)
+
+print("Full liste:", liste)
+print("İlk film:", liste[0])
+print("Son film:", liste[-1])
+
+
+
+Vraag2: 
